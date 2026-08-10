54d99983102eb907ba857b22c49bbb9c4cc52955
Andy Zhang <z-c-ge@outlook.com>
2026-08-10 15:26:59 +0800

    div: disable newton division temporarily for speed at small scale

--- stp_number/_div.h
+++ stp_number/_div.h
@@ -1,5 +1,5 @@
 #include "stp_number.h"
 
-#define STP_DIV_NEWTON
+/*#define STP_DIV_NEWTON*/
 
 int _STP_Number_div_abs(const STP_Number* lhs, const STP_Number* rhs, STP_Number* quotient, STP_Number* remainder);
